38d539413fe4f15982d570728dcf455a1350561c
Hassan Dahroug <hassandahroog66@gmail.com>
2025-03-19 03:19:52 +0200

    Modify session logic

--- server/index.ts
+++ server/index.ts
@@ -13,8 +13,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    secure: false,
+    maxAge: 24 * 60 * 60 * 1000 * 30// 24 hours
   }
 }));
 
